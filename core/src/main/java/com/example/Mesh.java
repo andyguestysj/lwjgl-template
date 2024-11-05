afaa4fec3b5da24d15d3a463522bc18e879cc985
Andy Guest <a.guest@yorksj.ac.uk>
2024-11-05 11:08:24 +0000

    kj

--- core/src/main/java/com/example/Mesh.java
+++ core/src/main/java/com/example/Mesh.java
@@ -53,6 +53,7 @@ public class Mesh {
 			glBufferData(GL_ARRAY_BUFFER, positionsBuffer, GL_STATIC_DRAW);
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+			
 
             // Color VBO
 			vboId = glGenBuffers();
@@ -78,7 +79,46 @@ public class Mesh {
   
   }
 
-  public int getMeshID() { return vaoId;}
+	public Mesh(float[] positions, int[] indices) {
+    vertCount = indices.length;
+    
+    List<Integer> vboIdList;
+
+		try (MemoryStack stack = MemoryStack.stackPush()) {
+
+			vboIdList = new ArrayList<>();
+
+			vaoId = glGenVertexArrays();
+			glBindVertexArray(vaoId);
+
+			int vboId = glGenBuffers();
+			
+			vboIdList.add(vboId);
+			FloatBuffer positionsBuffer = stack.callocFloat(positions.length);
+			positionsBuffer.put(0,positions);
+			glBindBuffer(GL_ARRAY_BUFFER, vboId);
+			glBufferData(GL_ARRAY_BUFFER, positionsBuffer, GL_STATIC_DRAW);
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, false, 6*Float.BYTES, 0);
+
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 3, GL_FLOAT, false, 6*Float.BYTES, 3*Float.BYTES);
+
+			// Index VBO
+			vboId = glGenBuffers();
+			vboIdList.add(vboId);
+			IntBuffer indicesBuffer = stack.callocInt(indices.length);
+			indicesBuffer.put(0, indices);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboId);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+    }
+  
+  }
+	
+	public int getMeshID() { return vaoId;}
   public int getVertexCount() { return vertCount;}
 
 	
